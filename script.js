6cce9c91a797fafa0a6a28e4a4f58261560f7c56
Patrick Smyth <patricksmyth1990@proton.me>
2023-07-16 09:46:18 +0900

    Create UI version of rock paper scissors, using DOM manipulation. Use event listeners on buttons to capture human plays, and keep a tally of scores. Display a <p> with the winner after five rounds.

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let humanScore = 0;
+let computerScore = 0;
+let roundCount = 0;
+
+// This function generates a random number between 1-3 and returns
+// rock, paper, or scissors as the computer's play
 function getComputerChoice() {
-    let result = "";
+    let result;
     const randomNum = Math.floor(Math.random() * 3) + 1;
     if (randomNum == 1) {
         return result = "rock";
@@ -10,47 +16,143 @@ function getComputerChoice() {
     }
 }
 
+// This function plays one round of the game, comparing the player's 
+// choice and the computer's play and returning a string with the result.
 function playRound(playerSelection, computerSelection) {
-    console.log("The computer played " + computerSelection + ".");
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+        humanScore++;
         return "You win! Rock beats scissors.";
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+        computerScore++;
         return "You lose! Paper beats rock!";
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+        humanScore++;
         return "You win! Paper beats rock!";
     } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
+        computerScore++;
         return "You lose! Scissors beats paper!";
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+        humanScore++;
         return "You win! Scissors beats paper!";
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+        computerScore++;
         return "You lose! Rock beats scissors!";
     } else if (playerSelection.toLowerCase() == computerSelection) {
         return "It's a tie!";
     }
 }
 
-function game() {
-    let humanScore = 0;
-    let computerScore = 0;
+function uiGame() {
+    scores = "Your score: " + humanScore + "<br>" + "Computer score: " + computerScore;
+    document.getElementById("newline").innerHTML = scores;
     for (let i = 0; i < 5; i++) {
-        let input = prompt("Rock, Paper, or Scissors?");
-        let winOrLose = playRound(input, getComputerChoice());
-        if (winOrLose.startsWith("You win")) {
-            humanScore += 1;
-        } else if  (winOrLose.startsWith("You lose")) {
-            computerScore += 1;
-        }
-        console.log("Your score: " + humanScore);
-        console.log("Opponent's score: " + computerScore);
-    }
-    if (humanScore > computerScore) {
-        console.log("You won with " + humanScore + "! Your opponent only got " + computerScore + ".");
-    } else if (humanScore < computerScore) {
-        console.log("You lost! You only got " + humanScore + ", but your opponent got " + computerScore + ".");
-    } 
-    else if (humanScore == computerScore) {
-        console.log("It's a tie! You and your opponent both got " + humanScore + ".");
-    }
-}
+        if (roundCount == 5 && humanScore > computerScore) {
+            gameResult.textContent = "You won with " + humanScore + "! Your opponent only got " + computerScore + ".";
+            roundCount = 0;
+            humanScore = 0;
+            computerScore = 0;
+            //console.log("You won with " + humanScore + "! Your opponent only got " + computerScore + ".");
+        } else if (roundCount == 5 && humanScore < computerScore) {
+            gameResult.textContent = "You lost! You got " + humanScore + ", but your opponent got " + computerScore + ".";
+            roundCount = 0;
+            humanScore = 0;
+            computerScore = 0;
+            //console.log("You lost! You only got " + humanScore + ", but your opponent got " + computerScore + ".");
+        } else if (roundCount == 5 && humanScore == computerScore) {
+            gameResult.textContent = "It's a tie! You and your opponent both got " + humanScore + ".";
+            roundCount = 0;
+            humanScore = 0;
+            computerScore = 0;
+            //console.log("It's a tie! You and your opponent both got " + humanScore + ".");
+        };
+    };
+    return;
+    //if (roundCount == 1) {
+    //    div.removeChild(gameResult);
+    //};
+};
+ 
+// This function goes through five rounds of the game, keeping score and
+// declaring the winner and points at the end.
+//function game() {
+    //for (let i = 0; i < 5; i++) {
+        //let input = prompt("Rock, Paper, or Scissors?");
+        //let winOrLose = playRound(input, getComputerChoice());
+        
+        
+        //console.log("Your score: " + humanScore);
+        //console.log("Opponent's score: " + computerScore);
+    //}
+    
+//}
 
-console.log(game());
+// console.log(game());
+
+const body = document.querySelector('body');
+const div = document.createElement('div');
+const newLine = document.createElement('p');
+const roundResult = document.createElement('p');
+const gameResult = document.createElement('p');
+
+body.appendChild(div);
+newLine.setAttribute('id', 'newline');
+div.appendChild(newLine);
+// I didn't need to use insertBefore() here, but it gives me a chance to
+// practice using it.
+div.insertBefore(roundResult, newLine);
+div.appendChild(gameResult);
+
+let scores = "Your score: " + humanScore + "<br>" + "Computer score: " + computerScore;
+document.getElementById("newline").innerHTML = scores;
+
+const rock = document.querySelector('#rock-button');
+rock.addEventListener('click', () => { 
+    roundResult.textContent = playRound('rock', getComputerChoice());
+    roundCount += 1;
+    //if (roundResult.textContent.startsWith("You win")) {
+    //    humanScore += 1;
+    //} else if  (roundResult.textContent.startsWith("You lose")) {
+    //    computerScore += 1;
+    //}
+    uiGame();
+});
+
+const paper = document.querySelector('#paper-button');
+paper.addEventListener('click', () => { 
+    roundResult.textContent = playRound('paper', getComputerChoice());
+    roundCount += 1;
+    //if (roundResult.textContent.startsWith("You win")) {
+    //    humanScore += 1;
+    //} else if  (roundResult.textContent.startsWith("You lose")) {
+    //    computerScore += 1;
+    //}
+    uiGame();
+});
+
+const scissors = document.querySelector('#scissors-button');
+scissors.addEventListener('click', () => { 
+    roundResult.textContent = playRound('scissors', getComputerChoice());
+    roundCount += 1;
+    //if (roundResult.textContent.startsWith("You win")) {
+    //    humanScore += 1;
+    //} else if  (roundResult.textContent.startsWith("You lose")) {
+    //    computerScore += 1;
+    //}
+    uiGame();
+});
+
+
+
+/* if (roundCount == 5 && humanScore > computerScore) {
+    gameResult.textContent = "You won with " + humanScore + "! Your opponent only got " + computerScore + ".";
+    roundCount = 0;
+    //console.log("You won with " + humanScore + "! Your opponent only got " + computerScore + ".");
+} else if (roundCount == 5 && humanScore < computerScore) {
+    gameResult.textContent = "You lost! You only got " + humanScore + ", but your opponent got " + computerScore + ".";
+    roundCount = 0;
+    //console.log("You lost! You only got " + humanScore + ", but your opponent got " + computerScore + ".");
+} else if (roundCount == 5 && humanScore == computerScore) {
+    gameResult.textContent = "It's a tie! You and your opponent both got " + humanScore + ".";
+    roundCount = 0;
+    //console.log("It's a tie! You and your opponent both got " + humanScore + ".");
+}; */
